1d22f6605b76cd614ee4d7544998a7adf38664b5
rannl-1980 <76993369+rannl-1980@users.noreply.github.com>
2021-12-20 19:40:13 +0200

    Create start.sh

---
+++ applications/chronos-client/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$CHRONOS_SERVER_DNS" == "None" ]; then
+    CHRONOS_SERVER_DNS="chronos-server.$DOMAIN_NAME"
+fi
+
+cd $ARTIFACTS_PATH
+python3 client.py $CHRONOS_SERVER_DNS > /dev/null &
