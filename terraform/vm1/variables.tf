0f50ca3956b32fb4dc6ac1feb4853619e2dbe407
rannl-1980 <76993369+rannl-1980@users.noreply.github.com>
2022-04-18 17:09:00 +0300

    Update variables.tf

--- terraform/vm1/variables.tf
+++ terraform/vm1/variables.tf
@@ -12,3 +12,12 @@ variable "server" {
   type = string
   default = "192.168.42.110"
 }
+
+variable "virtual_machine_template_name" {
+  type = string
+}
+
+variable "virtual_machine_name" {
+  type = string
+  default = "vm started by a script"
+}
