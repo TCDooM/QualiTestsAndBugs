38ae499aece088508554aa870cf331dfadc0664b
rannl-1980 <76993369+rannl-1980@users.noreply.github.com>
2022-04-19 17:38:00 +0300

    Update main.tf

--- terraform/vm-in-out/main.tf
+++ terraform/vm-in-out/main.tf
@@ -56,19 +56,3 @@ resource "vsphere_virtual_machine" "vm" {
 
   }
 }
-
-output "vm_ip_addr" {
-  value = vsphere_virtual_machine.vm.default_ip_address
-  description = "The default IP address of the raised VM"
-}
-
-output "vm_memory" {
-  value = vsphere_virtual_machine.vm.memory
-  description = "The memory of the raised VM"
-}
-
-output "vm_tools_status" {
-  value = vsphere_virtual_machine.vm.vmware_tools_status
-  description = "The state of vmware tools the raised VM"
-}
-
