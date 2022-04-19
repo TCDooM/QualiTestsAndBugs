dd3307bd2e65b62fce1060af1bb5685b0c9cae37
rannl-1980 <76993369+rannl-1980@users.noreply.github.com>
2022-04-19 14:21:59 +0300

    Update main.tf

--- terraform/vm-in-out/main.tf
+++ terraform/vm-in-out/main.tf
@@ -56,3 +56,8 @@ resource "vsphere_virtual_machine" "vm" {
 
   }
 }
+
+output "vm_ip_addr" {
+  value = vsphere_virtual_machine.default_ip_address
+  description = "The default IP address of the raised VM"
+}
