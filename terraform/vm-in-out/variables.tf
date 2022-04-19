5872d644183d88448a3d20a658b07cbdcda65777
rannl-1980 <76993369+rannl-1980@users.noreply.github.com>
2022-04-19 12:11:33 +0300

    Create variables.tf

---
+++ terraform/vm-in-out/variables.tf
@@ -0,0 +1,24 @@
+variable "private_cloud_login" {
+  type = string
+  sensitive = true
+}
+
+variable "private_cloud_password" {
+  type = string
+  sensitive = true
+}
+
+variable "private_cloud_host" {
+  type = string
+  sensitive = true
+}
+
+variable "virtual_machine_template_name" {
+  type = string
+  default = "test1"
+}
+
+variable "virtual_machine_name" {
+  type = string
+  default = "vm_from_torque"
+}
