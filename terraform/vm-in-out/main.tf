55ca2ee67a9ef3b3068a5d5e649a62ad4c49e3fc
rannl-1980 <76993369+rannl-1980@users.noreply.github.com>
2022-04-19 17:24:17 +0300

    Update main.tf

--- terraform/vm-in-out/main.tf
+++ terraform/vm-in-out/main.tf
@@ -66,3 +66,9 @@ output "vm_memory" {
   value = vsphere_virtual_machine.vm.memory
   description = "The memory of the raised VM"
 }
+
+output "vm_tools_status" {
+  value = vsphere_virtual_machine.vm.vmware_tools_status
+  description = "The state of vmware tools the raised VM"
+}
+
